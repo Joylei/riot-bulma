f35b442da0ce8b88f3d74c5b2e8079359a8e9ae2
joylei <leingliu@126.com>
2016-12-28 00:45:41 +0800

    fix some styles

--- src/app/lib/preloader.js
+++ src/app/lib/preloader.js
@@ -1,21 +1,24 @@
 import 'nprogress/nprogress.css'
 import NProgress from 'nprogress'
-import {observable} from 'riot'
+import {
+    observable
+} from 'riot'
 import http from './http'
 //import {ready} from './dom'
 
-function createLoader(){
+function createLoader() {
     //pending operations
     let pendingCount = 0
     let timerId
     let startTime = 0
-    function inc(){
+
+    function inc() {
         pendingCount++
-        if(!NProgress.isStarted()){
+        if (!NProgress.isStarted()) {
             NProgress.start()
             startTime = new Date()
         }
-        if(timerId){
+        if (timerId) {
             clearTimeout(timerId)
             timerId = null
         }
@@ -24,20 +27,22 @@ function createLoader(){
     function dec() {
         NProgress.inc()
         pendingCount--
-        if(pendingCount > 0) return
+        if (pendingCount > 0) return
 
         pendingCount = 0
-        if(!timerId){
+        if (!timerId) {
             let eclapsed = new Date() - startTime
-            timerId = setTimeout(()=>{
+            timerId = setTimeout(() => {
                 NProgress.done()
-                //NProgress.remove()
+                    //NProgress.remove()
             }, eclapsed > 1000 ? 300 : 600)
         }
     }
 
-    function init(){
-        NProgress.configure({ showSpinner: false })
+    function init() {
+        NProgress.configure({
+            showSpinner: false
+        })
 
         //wait page ready event
         //inc()
@@ -57,8 +62,7 @@ function createLoader(){
 let loader = createLoader()
 
 //hook http events
-http.onstart = ()=> loader.inc()
-http.oncomplete = ()=> loader.dec()
+http.onstart = () => loader.inc()
+http.oncomplete = () => loader.dec()
 
 export default loader
-
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,6 @@
 import {observable} from 'riot'
 import modal from './lib/modal'
+import {isObject} from '../shared/util'
 
 export const Action_Login = 'login'
 export const Action_Logout = 'logout'
@@ -34,38 +35,20 @@ const state = {
      * @param {string} mode optional
      */
     toast(type, title, mode){
-        let item
-        if(Object.prototype.toString.call(type) === '[object Object]'){
-            item = type
-        }else if(type){
-            item = { type, title, mode }
-        }
+        let item = isObject(type) ? type : { type, title, mode }
 
-        if(item){
-            this.toasts.push(item)
+        this.toasts.push(item)
+        this.trigger(Action_Toast, this.toasts)
+        //remove it
+        setTimeout(()=>{
+            let index = this.toasts.indexOf(item)
+            this.toasts.splice(index, 1)
             this.trigger(Action_Toast, this.toasts)
-            //remove it
-            setTimeout(()=>{
-                let index = this.toasts.indexOf(item)
-                this.toasts.splice(index, 1)
-                this.trigger(Action_Toast, this.toasts)
-            }, 1500)
-        }
+        }, 1500)
     }
 }
 
 modal(state)
-
-// function createStore(obj){
-//     let store = observable(obj)
-//     Object.keys(store).forEach(key =>{
-//         let val = store[key]
-//         if(typeof val === 'function'){
-//             store[key] = val.bind(store)
-//         }
-//     })
-//     return store
-// }
 
 const store = observable(state)
 export default store
